30e5013bfab4ff9805a7e555edebad8cdc244d7b
Maciej Barć <xgqt@protonmail.com>
2020-09-16 17:52:34 +0200

    ci: add support for pylint
    src: fix QA issues

--- test.sh
+++ test.sh
@@ -1,23 +1,41 @@
 #!/bin/sh
 
 
+# Original author: XGQT
+# Licensed under the ISC License
+# Copyright (c) 2020, src_prepare group
+
+
+# $1 - code check tool
+# $2 - files to check (quoed list)
+# Examples:
+#  - run_test shellcheck "src/check-commit src/repomanci"
+#  - run_test pylint "${py_files}"
+
 exit_result=0
-files="$(grep -R --exclude-dir='.git' '^#!/.*sh$' 2>/dev/null | cut -d ':' -f 1)"
+
+run_test() {
+    for file in ${2}
+    do
+        echo "File ${file}... checking"
+        if command "${1}" "${file}"
+        then
+            echo "    file is correct"
+        else
+            echo "    there were errors found in the file"
+            exit_result=1
+        fi
+        echo "Done: ${file}"
+        echo
+    done
+}
 
 
-for file in ${files}
-do
-    echo "File ${file}... checking"
-    if shellcheck "${file}"
-    then
-        echo "    file is correct"
-    else
-        echo "    there were errors found in the file"
-        exit_result=1
-    fi
-    echo "Done: ${file}"
-    echo
-done
+sh_files="$(grep -R --exclude-dir='.git' '^#!/.*sh$' 2>/dev/null | cut -d ':' -f 1)"
+py_files="$(grep -R --exclude-dir='.git' '^#!/.*python$' 2>/dev/null | cut -d ':' -f 1)"
+
+run_test shellcheck "${sh_files}"
+run_test pylint "${py_files}"
 
 if [ ${exit_result} = 0 ]
 then
